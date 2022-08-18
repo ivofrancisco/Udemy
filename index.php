d751a90caa858c112902ddc9fa52f41984972eda
Ivo Francisco <mazivo.mun@gmail.com>
2022-08-18 17:50:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+    <h1>Hello World!</h1>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
